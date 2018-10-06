2b764e9c58b216f135abd6dd7305b3b4bb64c968
Andrea Caforio <andrea.caforio@epfl.ch>
2018-10-06 21:20:26 +0200

    boneh poc

--- primitives/hibe/boneh.go
+++ primitives/hibe/boneh.go
@@ -6,11 +6,12 @@ package hibe
 import (
 	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 
 	"github.com/Nik-U/pbc"
 )
 
-const maxDepth = 100
+const maxDepth = 50
 
 type Boneh struct{}
 
@@ -22,14 +23,26 @@ type bonehParams struct {
 type bonehEntity struct {
 	ID [][]byte
 
+	// FIXME
+	G, G3 *pbc.Element
+	H     [maxDepth]*pbc.Element
+
 	A0, A1 *pbc.Element
 	B      []*pbc.Element
+}
+
+type bonehCiphertext struct {
+	V, W *pbc.Element
 }
 
 // NewBoneh creates a fresh protocol instance over a symmetric pairing.
 func NewBoneh() *Boneh {
 	return &Boneh{}
 }
+
+var (
+	g1, g2, ss *pbc.Element
+)
 
 func (b Boneh) Setup(seed []byte) (params, root []byte, err error) {
 	hash := func(base []byte, n int) []byte {
@@ -46,11 +59,14 @@ func (b Boneh) Setup(seed []byte) (params, root []byte, err error) {
 	alpha := pairing.NewZr().SetFromHash(hash(seed, 2))
 
 	G1 := pairing.NewG1().MulZn(G, alpha)
+	g1 = G1
 	G2 := pairing.NewG1().SetFromHash(hash(seed, 3))
+	g2 = G2
 	G3 := pairing.NewG1().SetFromHash(hash(seed, 4))
 
 	r := pairing.NewZr().SetFromHash(hash(seed, 5))
 	A0 := pairing.NewG1().MulZn(G2, alpha)
+	A0 = pairing.NewG1().Add(A0, pairing.NewG1().MulZn(G3, r))
 	A1 := pairing.NewG1().MulZn(G, r)
 
 	var H [maxDepth]*pbc.Element
@@ -68,11 +84,97 @@ func (b Boneh) Setup(seed []byte) (params, root []byte, err error) {
 
 	e := bonehEntity{
 		ID: [][]byte{},
+		G:  G, G3: G3,
+		H:  H,
 		A0: A0, A1: A1,
 		B: B[:],
 	}
 	root, err = e.MarshalJSON()
 	return
+}
+
+func (b Boneh) Extract(ancestor, id []byte) ([]byte, error) {
+	var e bonehEntity
+	if err := e.UnmarshalJSON(ancestor); err != nil {
+		return nil, err
+	}
+
+	childID := append(e.ID, id)
+	k := len(childID)
+	t := pairing.NewZr().Rand()
+
+	h := pairing.NewG1().Set1()
+	for i := 0; i < k; i++ {
+		j := pairing.NewZr().SetFromStringHash(string(childID[i]), sha256.New())
+		h = pairing.NewG1().Add(h, pairing.NewG1().MulZn(e.H[i], j))
+	}
+	h = pairing.NewG1().Add(h, e.G3)
+	h = pairing.NewG1().MulZn(h, t)
+
+	ik := pairing.NewZr().SetFromStringHash(string(id), sha256.New())
+	A0 := pairing.NewG1().Add(e.A0, pairing.NewG1().MulZn(e.B[0], ik))
+	A0 = pairing.NewG1().Add(A0, h)
+
+	A1 := pairing.NewG1().Add(e.A1, pairing.NewG1().MulZn(e.G, t))
+
+	var B []*pbc.Element
+	for i := k; i < maxDepth; i++ {
+		B = append(B, pairing.NewG1().Add(e.B[i-k+1], pairing.NewG1().MulZn(e.H[i], t)))
+	}
+
+	child := &bonehEntity{
+		ID: childID,
+		G:  e.G, G3: e.G3,
+		H:  e.H,
+		A0: A0, A1: A1,
+		B: B,
+	}
+	return child.MarshalJSON()
+}
+
+func (b Boneh) Encrypt(params, message []byte, id [][]byte) (c1, c2 []byte, err error) {
+	var p bonehParams
+	if err := p.UnmarshalJSON(params); err != nil {
+		return nil, nil, err
+	}
+
+	s := pairing.NewZr().Rand()
+	ss = s
+	h := pairing.NewGT().MulZn(pairing.NewGT().Pair(p.G1, p.G2), s).Bytes()
+	c1 = xor(message, h)
+
+	V := pairing.NewG1().MulZn(p.G, s)
+
+	W := pairing.NewG1().Set1()
+	for i := 0; i < len(id); i++ {
+		j := pairing.NewZr().SetFromStringHash(string(id[i]), sha256.New())
+		W = pairing.NewG1().Add(W, pairing.NewG1().MulZn(p.H[i], j))
+	}
+	W = pairing.NewG1().Add(W, p.G3)
+	W = pairing.NewG1().MulZn(W, s)
+
+	ct := bonehCiphertext{V: V, W: W}
+	c2, err = ct.MarshalJSON()
+	return
+}
+
+func (b Boneh) Decrypt(entity, c1, c2 []byte) ([]byte, error) {
+	var e bonehEntity
+	if err := e.UnmarshalJSON(entity); err != nil {
+		return nil, err
+	}
+
+	var c bonehCiphertext
+	if err := c.UnmarshalJSON(c2); err != nil {
+		return nil, err
+	}
+
+	fmt.Println(pairing.NewGT().MulZn(pairing.NewGT().Pair(g1, g2), ss).Bytes())
+
+	n := pairing.NewGT().Pair(e.A1, c.W)
+	d := pairing.NewGT().Pair(c.V, e.A0)
+	fmt.Println(pairing.NewGT().Sub(n, d).Bytes())
+	return xor(c1, pairing.NewGT().Sub(d, n).Bytes()), nil
 }
 
 // bonehParamsPacket is a helper structure that enables marshalling.
@@ -111,14 +213,24 @@ func (p *bonehParams) UnmarshalJSON(data []byte) error {
 
 type bonehEntityPacket struct {
 	ID     [][]byte
+	G, G3  []byte
+	H      [][]byte
 	A0, A1 []byte
 	B      [][]byte
 }
 
 func (e bonehEntity) MarshalJSON() ([]byte, error) {
-	packet := bonehEntityPacket{ID: e.ID, A0: e.A0.CompressedBytes(), A1: e.A1.CompressedBytes()}
+	packet := bonehEntityPacket{
+		ID: e.ID,
+		G:  e.G.CompressedBytes(),
+		G3: e.G3.CompressedBytes(),
+		A0: e.A0.CompressedBytes(), A1: e.A1.CompressedBytes(),
+	}
 	for _, b := range e.B {
 		packet.B = append(packet.B, b.CompressedBytes())
+	}
+	for _, h := range e.H {
+		packet.H = append(packet.H, h.CompressedBytes())
 	}
 	return json.Marshal(&packet)
 }
@@ -129,10 +241,35 @@ func (e *bonehEntity) UnmarshalJSON(data []byte) error {
 		return err
 	}
 	e.ID = packet.ID
+	e.G = pairing.NewG1().SetCompressedBytes(packet.G)
+	e.G3 = pairing.NewG1().SetCompressedBytes(packet.G3)
 	e.A0 = pairing.NewG1().SetCompressedBytes(packet.A0)
 	e.A1 = pairing.NewG1().SetCompressedBytes(packet.A1)
 	for _, b := range packet.B {
 		e.B = append(e.B, pairing.NewG1().SetCompressedBytes(b))
 	}
+	for i, h := range packet.H {
+		e.H[i] = pairing.NewG1().SetCompressedBytes(h)
+	}
+	return nil
+}
+
+// bonehCiphertextPacket is a helper structure that enables marshalling.
+type bonehCiphertextPacket struct {
+	V, W []byte
+}
+
+func (e bonehCiphertext) MarshalJSON() ([]byte, error) {
+	packet := bonehCiphertextPacket{V: e.V.CompressedBytes(), W: e.W.CompressedBytes()}
+	return json.Marshal(&packet)
+}
+
+func (e *bonehCiphertext) UnmarshalJSON(data []byte) error {
+	var packet bonehCiphertextPacket
+	if err := json.Unmarshal(data, &packet); err != nil {
+		return err
+	}
+	e.V = pairing.NewG1().SetCompressedBytes(packet.V)
+	e.W = pairing.NewG1().SetCompressedBytes(packet.W)
 	return nil
 }
--- primitives/hibe/boneh_test.go
+++ primitives/hibe/boneh_test.go
@@ -1,8 +1,11 @@
+// (c) 2018 EPFL
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
 package hibe
 
 import (
+	"bytes"
 	"crypto/rand"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,5 +21,80 @@ func TestBoneh(t *testing.T) {
 
 	params, root, err := b.Setup(seed[:])
 	require.Nil(err)
-	fmt.Println(params, root)
+
+	id1 := [][]byte{[]byte{1}}
+	id2 := [][]byte{[]byte{2}}
+	id21 := [][]byte{[]byte{2}, []byte{2, 1}}
+	id11 := [][]byte{[]byte{1}, []byte{1, 1}}
+	id111 := [][]byte{[]byte{1}, []byte{1, 1}, []byte{1, 1, 1}}
+	id1111 := [][]byte{[]byte{1}, []byte{1, 1}, []byte{1, 1, 1}, []byte{1, 1, 1, 1}}
+
+	msg := []byte("hello")
+
+	// Message to id1
+	e1, err := b.Extract(root, id1[0])
+	require.Nil(err)
+
+	c1, c2, err := b.Encrypt(params, msg, id1)
+	require.Nil(err)
+
+	pt, err := b.Decrypt(e1, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
+	// Message to id2
+	e2, err := b.Extract(root, id2[0])
+	require.Nil(err)
+
+	c1, c2, err = b.Encrypt(params, msg, id2)
+	require.Nil(err)
+
+	pt, err = b.Decrypt(e2, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
+	// Message to id21
+	e21, err := b.Extract(e2, id21[1])
+	require.Nil(err)
+
+	c1, c2, err = b.Encrypt(params, msg, id21)
+	require.Nil(err)
+
+	pt, err = b.Decrypt(e21, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
+	// Message to id11
+	e11, err := b.Extract(e1, id11[1])
+	require.Nil(err)
+
+	c1, c2, err = b.Encrypt(params, msg, id11)
+	require.Nil(err)
+
+	pt, err = b.Decrypt(e11, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
+	// Message to id111
+	e111, err := b.Extract(e11, id111[2])
+	require.Nil(err)
+
+	c1, c2, err = b.Encrypt(params, msg, id111)
+	require.Nil(err)
+
+	pt, err = b.Decrypt(e111, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
+	// Message to id1111
+	e1111, err := b.Extract(e111, id1111[3])
+	require.Nil(err)
+
+	c1, c2, err = b.Encrypt(params, msg, id1111)
+	require.Nil(err)
+
+	pt, err = b.Decrypt(e1111, c1, c2)
+	require.Nil(err)
+	require.True(bytes.Equal(msg, pt))
+
 }
